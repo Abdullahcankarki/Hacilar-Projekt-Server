9e369f8e54c2c475a40c0f8409cdcf9b45f4c052
Abdullahcan Karki <abdullahcankarki@MacBook-Pro-von-Abdullahcan-5.local>
2025-08-19 12:19:41 +0300

    Portänderung

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
 /* istanbul ignore file */
 
 import dotenv from "dotenv";
-dotenv.config() // read ".env"
+dotenv.config(); // read ".env"
 
 import http from "http";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import app from "./app";
-import { logger } from "./logger"
+import { logger } from "./logger";
 
 async function setup() {
+  let mongodURI = process.env.DB_CONNECTION_STRING;
+  if (!mongodURI) {
+    logger.error(
+      `Cannot start, no database configured. Set environment variable DB_CONNECTION_STRING. Use "memory" for MongoMemoryServer`
+    );
+    process.exit(1);
+  }
+  if (mongodURI === "memory") {
+    logger.info("Start MongoMemoryServer");
+    const MMS = await import("mongodb-memory-server");
+    const mongo = await MMS.MongoMemoryServer.create();
+    mongodURI = mongo.getUri();
+  }
 
-    let mongodURI = process.env.DB_CONNECTION_STRING;
-    if (!mongodURI) {
-        logger.error(`Cannot start, no database configured. Set environment variable DB_CONNECTION_STRING. Use "memory" for MongoMemoryServer`);
-        process.exit(1);
-    }
-    if (mongodURI === "memory") {
-        logger.info("Start MongoMemoryServer")
-        const MMS = await import('mongodb-memory-server')
-        const mongo = await MMS.MongoMemoryServer.create();
-        mongodURI = mongo.getUri();
-    }
+  logger.info(`Connect to mongod`);
+  await mongoose.connect(mongodURI);
 
-    logger.info(`Connect to mongod`)
-    await mongoose.connect(mongodURI);
-
-    const port = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 3000;
-    const httpServer = http.createServer(app);
-    httpServer.listen(port, () => {
-        logger.info(`Listening for HTTP at http://localhost:${port}`);
-    });
-
-};
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  const httpServer = http.createServer(app);
+  httpServer.listen(port, "0.0.0.0", () => {
+    logger.info(`Listening for HTTP at http://0.0.0.0:${port}`);
+  });
+}
 
 setup();
